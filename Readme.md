aa29be8b882fa1c55488911ade310698b957a096
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# How to accompany a chart, its pane or series point by text or image annotations
+
+
+<p>This example demonstrates how text and image <a href="http://help.devexpress.com/XtraCharts/CustomDocument7857.aspx">annotations</a> can be created and anchored to a chart, pane or series point.</p><p>See also:<br />
+- <a href="https://www.devexpress.com/Support/Center/p/E126">How to show a tooltip with a series point's data</a>; <br />
+- <a href="https://www.devexpress.com/Support/Center/p/E2483">How to show series labels for hot-tracked points</a>.</p>
+
+<br/>
+
+
---
+++ VB/AnnotationsSample/Form1.Designer.vb
@@ -0,0 +1,154 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Namespace AnnotationsSample
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+            Dim XyDiagram1 As DevExpress.XtraCharts.XYDiagram = New DevExpress.XtraCharts.XYDiagram()
+            Dim XyDiagramPane1 As DevExpress.XtraCharts.XYDiagramPane = New DevExpress.XtraCharts.XYDiagramPane()
+            Dim SecondaryAxisX1 As DevExpress.XtraCharts.SecondaryAxisX = New DevExpress.XtraCharts.SecondaryAxisX()
+            Dim SecondaryAxisY1 As DevExpress.XtraCharts.SecondaryAxisY = New DevExpress.XtraCharts.SecondaryAxisY()
+            Dim Series1 As DevExpress.XtraCharts.Series = New DevExpress.XtraCharts.Series()
+            Dim SideBySideBarSeriesLabel1 As DevExpress.XtraCharts.SideBySideBarSeriesLabel = New DevExpress.XtraCharts.SideBySideBarSeriesLabel()
+            Dim SeriesPoint1 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("A", New Object() {CType(15.0R, Object)})
+            Dim SeriesPoint2 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("B", New Object() {CType(6.0R, Object)})
+            Dim SeriesPoint3 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("C", New Object() {CType(19.0R, Object)})
+            Dim SeriesPoint4 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("D", New Object() {CType(9.0R, Object)})
+            Dim SeriesPoint5 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("E", New Object() {CType(21.0R, Object)})
+            Dim SeriesPoint6 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("F", New Object() {CType(13.0R, Object)})
+            Dim Series2 As DevExpress.XtraCharts.Series = New DevExpress.XtraCharts.Series()
+            Dim PointSeriesLabel1 As DevExpress.XtraCharts.PointSeriesLabel = New DevExpress.XtraCharts.PointSeriesLabel()
+            Dim SeriesPoint7 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("1", New Object() {CType(240.0R, Object)})
+            Dim SeriesPoint8 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("2", New Object() {CType(160.0R, Object)})
+            Dim SeriesPoint9 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("3", New Object() {CType(260.0R, Object)})
+            Dim SeriesPoint10 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("4", New Object() {CType(190.0R, Object)})
+            Dim SeriesPoint11 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("5", New Object() {CType(310.0R, Object)})
+            Dim SeriesPoint12 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("6", New Object() {CType(170.0R, Object)})
+            Dim SplineSeriesView1 As DevExpress.XtraCharts.SplineSeriesView = New DevExpress.XtraCharts.SplineSeriesView()
+            Dim SideBySideBarSeriesLabel2 As DevExpress.XtraCharts.SideBySideBarSeriesLabel = New DevExpress.XtraCharts.SideBySideBarSeriesLabel()
+            Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
+            CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(XyDiagram1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(XyDiagramPane1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(SecondaryAxisX1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(SecondaryAxisY1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Series1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(SideBySideBarSeriesLabel1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Series2, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(PointSeriesLabel1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(SplineSeriesView1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(SideBySideBarSeriesLabel2, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            '
+            'chartControl1
+            '
+            XyDiagram1.AxisX.Range.AlwaysShowZeroLevel = True
+            XyDiagram1.AxisX.Range.ScrollingRange.SideMarginsEnabled = True
+            XyDiagram1.AxisX.Range.SideMarginsEnabled = True
+            XyDiagram1.AxisX.VisibleInPanesSerializable = "-1"
+            XyDiagram1.AxisY.Range.AlwaysShowZeroLevel = True
+            XyDiagram1.AxisY.Range.ScrollingRange.SideMarginsEnabled = True
+            XyDiagram1.AxisY.Range.SideMarginsEnabled = True
+            XyDiagram1.AxisY.VisibleInPanesSerializable = "-1"
+            XyDiagram1.EnableAxisXScrolling = True
+            XyDiagramPane1.Name = "Pane 1"
+            XyDiagramPane1.PaneID = 0
+            XyDiagram1.Panes.AddRange(New DevExpress.XtraCharts.XYDiagramPane() {XyDiagramPane1})
+            SecondaryAxisX1.Alignment = DevExpress.XtraCharts.AxisAlignment.Near
+            SecondaryAxisX1.AxisID = 0
+            SecondaryAxisX1.DateTimeScaleOptions.ScaleMode = DevExpress.XtraCharts.ScaleMode.Manual
+            SecondaryAxisX1.Name = "Secondary AxisX 1"
+            SecondaryAxisX1.Range.AlwaysShowZeroLevel = True
+            SecondaryAxisX1.Range.Auto = False
+            SecondaryAxisX1.Range.MaxValueSerializable = "5"
+            SecondaryAxisX1.Range.MinValueSerializable = "1"
+            SecondaryAxisX1.Range.ScrollingRange.SideMarginsEnabled = True
+            SecondaryAxisX1.Range.SideMarginsEnabled = True
+            SecondaryAxisX1.VisibleInPanesSerializable = "0"
+            XyDiagram1.SecondaryAxesX.AddRange(New DevExpress.XtraCharts.SecondaryAxisX() {SecondaryAxisX1})
+            SecondaryAxisY1.Alignment = DevExpress.XtraCharts.AxisAlignment.Near
+            SecondaryAxisY1.AxisID = 0
+            SecondaryAxisY1.GridLines.Visible = True
+            SecondaryAxisY1.Name = "Secondary AxisY 1"
+            SecondaryAxisY1.Range.AlwaysShowZeroLevel = True
+            SecondaryAxisY1.Range.ScrollingRange.SideMarginsEnabled = True
+            SecondaryAxisY1.Range.SideMarginsEnabled = True
+            SecondaryAxisY1.VisibleInPanesSerializable = "0"
+            XyDiagram1.SecondaryAxesY.AddRange(New DevExpress.XtraCharts.SecondaryAxisY() {SecondaryAxisY1})
+            Me.chartControl1.Diagram = XyDiagram1
+            Me.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.chartControl1.Legend.Visible = False
+            Me.chartControl1.Location = New System.Drawing.Point(0, 0)
+            Me.chartControl1.Name = "chartControl1"
+            SideBySideBarSeriesLabel1.LineVisible = True
+            Series1.Label = SideBySideBarSeriesLabel1
+            Series1.LabelsVisibility = DevExpress.Utils.DefaultBoolean.[False]
+            Series1.Name = "Series 1"
+            Series1.Points.AddRange(New DevExpress.XtraCharts.SeriesPoint() {SeriesPoint1, SeriesPoint2, SeriesPoint3, SeriesPoint4, SeriesPoint5, SeriesPoint6})
+            PointSeriesLabel1.LineVisible = True
+            Series2.Label = PointSeriesLabel1
+            Series2.LabelsVisibility = DevExpress.Utils.DefaultBoolean.[False]
+            Series2.Name = "Series 2"
+            Series2.Points.AddRange(New DevExpress.XtraCharts.SeriesPoint() {SeriesPoint7, SeriesPoint8, SeriesPoint9, SeriesPoint10, SeriesPoint11, SeriesPoint12})
+            SplineSeriesView1.AxisXName = "Secondary AxisX 1"
+            SplineSeriesView1.AxisYName = "Secondary AxisY 1"
+            SplineSeriesView1.PaneName = "Pane 1"
+            Series2.View = SplineSeriesView1
+            Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series() {Series1, Series2}
+            SideBySideBarSeriesLabel2.LineVisible = True
+            Me.chartControl1.SeriesTemplate.Label = SideBySideBarSeriesLabel2
+            Me.chartControl1.Size = New System.Drawing.Size(677, 484)
+            Me.chartControl1.TabIndex = 0
+            '
+            'Form1
+            '
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(677, 484)
+            Me.Controls.Add(Me.chartControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            CType(XyDiagramPane1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(SecondaryAxisX1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(SecondaryAxisY1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(XyDiagram1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(SideBySideBarSeriesLabel1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Series1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(PointSeriesLabel1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(SplineSeriesView1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Series2, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(SideBySideBarSeriesLabel2, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+
+        End Sub
+
+		#End Region
+
+		Private chartControl1 As DevExpress.XtraCharts.ChartControl
+
+
+	End Class
+End Namespace
+
