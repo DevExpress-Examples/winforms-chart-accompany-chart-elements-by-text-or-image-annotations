8617fec270a9c1c8358398e5f2f3278df9c7ec29
DevExpressExampleBot <fakeEmail>
2022-11-23 16:42:41 +0400

    Source auto update [skip ci]

--- VB/AnnotationsSample/Form1.Designer.vb
+++ VB/AnnotationsSample/Form1.Designer.vb
@@ -1,5 +1,7 @@
-﻿Namespace AnnotationsSample
-    Partial Public Class Form1
+Namespace AnnotationsSample
+
+    Partial Class Form1
+
         ''' <summary>
         ''' Required designer variable.
         ''' </summary>
@@ -10,47 +12,47 @@
         ''' </summary>
         ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
-        #Region "Windows Form Designer generated code"
-
+#Region "Windows Form Designer generated code"
         ''' <summary>
         ''' Required method for Designer support - do not modify
         ''' the contents of this method with the code editor.
         ''' </summary>
         Private Sub InitializeComponent()
-            Dim xyDiagram1 As New DevExpress.XtraCharts.XYDiagram()
-            Dim xyDiagramPane1 As New DevExpress.XtraCharts.XYDiagramPane()
-            Dim secondaryAxisX1 As New DevExpress.XtraCharts.SecondaryAxisX()
-            Dim secondaryAxisY1 As New DevExpress.XtraCharts.SecondaryAxisY()
-            Dim series1 As New DevExpress.XtraCharts.Series()
-            Dim seriesPoint1 As New DevExpress.XtraCharts.SeriesPoint("A", New Object() { (DirectCast(15R, Object))})
-            Dim seriesPoint2 As New DevExpress.XtraCharts.SeriesPoint("B", New Object() { (DirectCast(6R, Object))})
-            Dim seriesPoint3 As New DevExpress.XtraCharts.SeriesPoint("C", New Object() { (DirectCast(19R, Object))})
-            Dim seriesPoint4 As New DevExpress.XtraCharts.SeriesPoint("D", New Object() { (DirectCast(9R, Object))})
-            Dim seriesPoint5 As New DevExpress.XtraCharts.SeriesPoint("E", New Object() { (DirectCast(21R, Object))})
-            Dim seriesPoint6 As New DevExpress.XtraCharts.SeriesPoint("F", New Object() { (DirectCast(13R, Object))})
-            Dim series2 As New DevExpress.XtraCharts.Series()
-            Dim seriesPoint7 As New DevExpress.XtraCharts.SeriesPoint("1", New Object() { (DirectCast(240R, Object))})
-            Dim seriesPoint8 As New DevExpress.XtraCharts.SeriesPoint("2", New Object() { (DirectCast(160R, Object))})
-            Dim seriesPoint9 As New DevExpress.XtraCharts.SeriesPoint("3", New Object() { (DirectCast(260R, Object))})
-            Dim seriesPoint10 As New DevExpress.XtraCharts.SeriesPoint("4", New Object() { (DirectCast(190R, Object))})
-            Dim seriesPoint11 As New DevExpress.XtraCharts.SeriesPoint("5", New Object() { (DirectCast(310R, Object))})
-            Dim seriesPoint12 As New DevExpress.XtraCharts.SeriesPoint("6", New Object() { (DirectCast(170R, Object))})
-            Dim splineSeriesView1 As New DevExpress.XtraCharts.SplineSeriesView()
+            Dim xyDiagram1 As DevExpress.XtraCharts.XYDiagram = New DevExpress.XtraCharts.XYDiagram()
+            Dim xyDiagramPane1 As DevExpress.XtraCharts.XYDiagramPane = New DevExpress.XtraCharts.XYDiagramPane()
+            Dim secondaryAxisX1 As DevExpress.XtraCharts.SecondaryAxisX = New DevExpress.XtraCharts.SecondaryAxisX()
+            Dim secondaryAxisY1 As DevExpress.XtraCharts.SecondaryAxisY = New DevExpress.XtraCharts.SecondaryAxisY()
+            Dim series1 As DevExpress.XtraCharts.Series = New DevExpress.XtraCharts.Series()
+            Dim seriesPoint1 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("A", New Object() {(CObj((15R)))})
+            Dim seriesPoint2 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("B", New Object() {(CObj((6R)))})
+            Dim seriesPoint3 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("C", New Object() {(CObj((19R)))})
+            Dim seriesPoint4 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("D", New Object() {(CObj((9R)))})
+            Dim seriesPoint5 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("E", New Object() {(CObj((21R)))})
+            Dim seriesPoint6 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("F", New Object() {(CObj((13R)))})
+            Dim series2 As DevExpress.XtraCharts.Series = New DevExpress.XtraCharts.Series()
+            Dim seriesPoint7 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("1", New Object() {(CObj((240R)))})
+            Dim seriesPoint8 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("2", New Object() {(CObj((160R)))})
+            Dim seriesPoint9 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("3", New Object() {(CObj((260R)))})
+            Dim seriesPoint10 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("4", New Object() {(CObj((190R)))})
+            Dim seriesPoint11 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("5", New Object() {(CObj((310R)))})
+            Dim seriesPoint12 As DevExpress.XtraCharts.SeriesPoint = New DevExpress.XtraCharts.SeriesPoint("6", New Object() {(CObj((170R)))})
+            Dim splineSeriesView1 As DevExpress.XtraCharts.SplineSeriesView = New DevExpress.XtraCharts.SplineSeriesView()
             Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
-            DirectCast(Me.chartControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(xyDiagram1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(xyDiagramPane1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(secondaryAxisX1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(secondaryAxisY1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(series1, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(series2, System.ComponentModel.ISupportInitialize).BeginInit()
-            DirectCast(splineSeriesView1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.chartControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((xyDiagram1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((xyDiagramPane1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((secondaryAxisX1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((secondaryAxisY1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((series1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((series2), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((splineSeriesView1), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
             ' 
             ' chartControl1
@@ -62,7 +64,7 @@
             xyDiagram1.EnableAxisXScrolling = True
             xyDiagramPane1.Name = "Pane 1"
             xyDiagramPane1.PaneID = 0
-            xyDiagram1.Panes.AddRange(New DevExpress.XtraCharts.XYDiagramPane() { xyDiagramPane1})
+            xyDiagram1.Panes.AddRange(New DevExpress.XtraCharts.XYDiagramPane() {xyDiagramPane1})
             secondaryAxisX1.Alignment = DevExpress.XtraCharts.AxisAlignment.Near
             secondaryAxisX1.AxisID = 0
             secondaryAxisX1.Name = "Secondary AxisX 1"
@@ -72,30 +74,30 @@
             secondaryAxisX1.VisualRange.MaxValueSerializable = "5"
             secondaryAxisX1.VisualRange.MinValueSerializable = "1"
             secondaryAxisX1.WholeRange.AutoSideMargins = True
-            xyDiagram1.SecondaryAxesX.AddRange(New DevExpress.XtraCharts.SecondaryAxisX() { secondaryAxisX1})
+            xyDiagram1.SecondaryAxesX.AddRange(New DevExpress.XtraCharts.SecondaryAxisX() {secondaryAxisX1})
             secondaryAxisY1.Alignment = DevExpress.XtraCharts.AxisAlignment.Near
             secondaryAxisY1.AxisID = 0
             secondaryAxisY1.GridLines.Visible = True
             secondaryAxisY1.Name = "Secondary AxisY 1"
             secondaryAxisY1.VisibleInPanesSerializable = "0"
             secondaryAxisY1.WholeRange.AutoSideMargins = True
-            xyDiagram1.SecondaryAxesY.AddRange(New DevExpress.XtraCharts.SecondaryAxisY() { secondaryAxisY1})
+            xyDiagram1.SecondaryAxesY.AddRange(New DevExpress.XtraCharts.SecondaryAxisY() {secondaryAxisY1})
             Me.chartControl1.Diagram = xyDiagram1
             Me.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill
-            Me.chartControl1.Legend.Visibility = DevExpress.Utils.DefaultBoolean.True
+            Me.chartControl1.Legend.Visibility = DevExpress.Utils.DefaultBoolean.[True]
             Me.chartControl1.Location = New System.Drawing.Point(0, 0)
             Me.chartControl1.Name = "chartControl1"
-            series1.LabelsVisibility = DevExpress.Utils.DefaultBoolean.False
+            series1.LabelsVisibility = DevExpress.Utils.DefaultBoolean.[False]
             series1.Name = "Series 1"
-            series1.Points.AddRange(New DevExpress.XtraCharts.SeriesPoint() { seriesPoint1, seriesPoint2, seriesPoint3, seriesPoint4, seriesPoint5, seriesPoint6})
-            series2.LabelsVisibility = DevExpress.Utils.DefaultBoolean.False
+            series1.Points.AddRange(New DevExpress.XtraCharts.SeriesPoint() {seriesPoint1, seriesPoint2, seriesPoint3, seriesPoint4, seriesPoint5, seriesPoint6})
+            series2.LabelsVisibility = DevExpress.Utils.DefaultBoolean.[False]
             series2.Name = "Series 2"
-            series2.Points.AddRange(New DevExpress.XtraCharts.SeriesPoint() { seriesPoint7, seriesPoint8, seriesPoint9, seriesPoint10, seriesPoint11, seriesPoint12})
+            series2.Points.AddRange(New DevExpress.XtraCharts.SeriesPoint() {seriesPoint7, seriesPoint8, seriesPoint9, seriesPoint10, seriesPoint11, seriesPoint12})
             splineSeriesView1.AxisXName = "Secondary AxisX 1"
             splineSeriesView1.AxisYName = "Secondary AxisY 1"
             splineSeriesView1.PaneName = "Pane 1"
             series2.View = splineSeriesView1
-            Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series() { series1, series2}
+            Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series() {series1, series2}
             Me.chartControl1.Size = New System.Drawing.Size(691, 473)
             Me.chartControl1.TabIndex = 0
             ' 
@@ -107,23 +109,19 @@
             Me.Controls.Add(Me.chartControl1)
             Me.Name = "Form1"
             Me.Text = "Form1"
-            DirectCast(xyDiagramPane1, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(secondaryAxisX1, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(secondaryAxisY1, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(xyDiagram1, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(series1, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(splineSeriesView1, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(series2, System.ComponentModel.ISupportInitialize).EndInit()
-            DirectCast(Me.chartControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((xyDiagramPane1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((secondaryAxisX1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((secondaryAxisY1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((xyDiagram1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((series1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((splineSeriesView1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((series2), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.chartControl1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
-
         End Sub
 
-        #End Region
-
+#End Region
         Private chartControl1 As DevExpress.XtraCharts.ChartControl
-
-
     End Class
 End Namespace
-
--- VB/AnnotationsSample/Form1.vb
+++ VB/AnnotationsSample/Form1.vb
@@ -1,15 +1,16 @@
-﻿#Region "#usings"
+#Region "#usings"
 Imports System
 Imports System.Drawing
 Imports System.IO
 Imports System.Windows.Forms
 Imports DevExpress.Drawing
 Imports DevExpress.XtraCharts
-' ...
-#End Region ' #usings
 
+' ...
+#End Region  ' #usings
 Namespace AnnotationsSample
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits Form
 
         Public Sub New()
@@ -17,62 +18,48 @@ Namespace AnnotationsSample
         End Sub
 
 #Region "#generalcode"
-        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
             ' Add a text annotation to the chart control's repository.
             chartControl1.AnnotationRepository.Add(New TextAnnotation("Annotation 1"))
-
             ' And, assign a series point to the annotation's AnchorPoint property.
             ' This adds the annotation to the series point's Annotations collection.
             chartControl1.AnnotationRepository(0).AnchorPoint = New SeriesPointAnchorPoint(chartControl1.Series(0).Points(2))
-
             ' Now, create an image annotation, and add it to the chart's collection.
             chartControl1.Annotations.AddImageAnnotation("Annotation 2", DXImage.FromStream(New FileStream("...\...\image.png", FileMode.Open, FileAccess.Read, FileShare.Read)))
-
             ' Define the X and Y absolute coordinates for the annotation, in pixels.
             CType(chartControl1.Annotations(0).AnchorPoint, ChartAnchorPoint).X = 150
             CType(chartControl1.Annotations(0).AnchorPoint, ChartAnchorPoint).Y = 150
-
             ' Obtain the additional pane from the diagram's collection.
             Dim myPane As XYDiagramPaneBase = CType(chartControl1.Diagram, XYDiagram).Panes(0)
-
             ' And, position the chart's annotation in this pane's right top corner;
             CType(chartControl1.Annotations(0).ShapePosition, FreePosition).DockTarget = myPane
             CType(chartControl1.Annotations(0).ShapePosition, FreePosition).DockCorner = DockCorner.RightTop
-
             ' Another annotation is now being added to the collection of this pane.
             myPane.Annotations.AddImageAnnotation("Annotation 3", DXImage.FromStream(New FileStream("...\...\image.png", FileMode.Open, FileAccess.Read, FileShare.Read)))
-
             ' Define its axis coordinates (in units appropriate for the scale type of the axes).
             CType(myPane.Annotations(0).AnchorPoint, PaneAnchorPoint).AxisXCoordinate.AxisValue = 2
             CType(myPane.Annotations(0).AnchorPoint, PaneAnchorPoint).AxisYCoordinate.AxisValue = 180
-
             ' Position the annotation in relation to its anchor point.
             CType(myPane.Annotations(0).ShapePosition, RelativePosition).Angle = -135
             CType(myPane.Annotations(0).ShapePosition, RelativePosition).ConnectorLength = 50
-
             ' You can get an annotation either via the collection of the element to which it is anchored,
             ' or centrally, via the chart control's repository (e.g. by its name).
             Dim myTextAnnotation As TextAnnotation = CType(chartControl1.AnnotationRepository.GetElementByName("Annotation 1"), TextAnnotation)
             Dim myImageAnnotation As ImageAnnotation = CType(chartControl1.AnnotationRepository.GetElementByName("Annotation 3"), ImageAnnotation)
-
             ' Define the text for the text annotation.
             myTextAnnotation.Text = "<i>Basic</i> <b>HTML</b> <u>is</u> <color=blue>supported</color>."
-
             ' Enable the interactive positioning for the image annotation.
             myImageAnnotation.RuntimeMoving = True
             myImageAnnotation.RuntimeAnchoring = True
             myImageAnnotation.RuntimeResizing = True
             myImageAnnotation.RuntimeRotation = True
-
             ' Specify image annotation size mode.
             myImageAnnotation.SizeMode = ChartImageSizeMode.Tile
-
             ' And, adjust image annotation appearance options.
             myImageAnnotation.ShapeKind = ShapeKind.RoundedRectangle
             myImageAnnotation.ShapeFillet = 10
             myImageAnnotation.ConnectorStyle = AnnotationConnectorStyle.Arrow
         End Sub
-#End Region ' #generalcode
-
+#End Region  ' #generalcode
     End Class
 End Namespace
